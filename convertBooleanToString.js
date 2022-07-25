0faee589973f578d0b17069bd45239ac71502c26
Aer-osa <eric.n.long@gmail.com>
2022-07-25 15:05:53 -0700

    Add Kata

--- convertBooleanToString.js
+++ convertBooleanToString.js
@@ -3,9 +3,18 @@
 // Note: Only valid inputs will be given.
 
 function booleanToString(b){
+  //Parameters Boolean value, "true" or "false"
+  //Returns "true" or "false"
+
     if(b === true){
       return "true"
     }else{
       return "false"
     }
   }
+  //booleanToString(true){
+    //   return b ? "true" : "false"
+    // return "true"
+  //}
+
+  //Pseudocode Function takes a boolean and converts it to either a "true" or "false" value.
