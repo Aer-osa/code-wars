ec22b743caf74925a55fd4cfb088195f54125d25
aer-long <eric.n.long+1@gmail.com>
2022-08-01 15:26:24 -0400

    Add Kata

---
+++ abreiviateToInitials.js
@@ -0,0 +1,20 @@
+function abbrevName(name){
+//Parameters
+  //x beginning name
+  //y ending initials
+  const firstLetters = name
+    .split(' ')
+    .map(word => word[0])
+    .join('.');
+
+  return firstLetters;
+// Returns initials  
+
+}
+//examples
+//abbrevName('Sam Harris'){
+//return S.H
+//}
+
+//Pseudocode
+// we have a function that takes the first letter of each word and puts it in a string seperated by a period
